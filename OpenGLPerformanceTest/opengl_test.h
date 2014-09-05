ffd64e3f962ed1c2984babe350da584e6b0c9582
Tiemo Jung <tiemo.jung@thm.de>
2014-09-05 09:59:48 +0200

    vertex buffer is now shared between all tests

--- OpenGLPerformanceTest/opengl_test.h
+++ OpenGLPerformanceTest/opengl_test.h
@@ -6,57 +6,43 @@
 
 #include <iterator>
 #include <vector>
+#include <memory>
+#include <tuple>
 
 #define NUMBER_OF_OBJECTS   1024 * 4
+
+struct shared_state {
+    vertex_buffer   _geometry;
+    int             _vertices_per_object;
+};
+
+std::shared_ptr<shared_state> get_shared_state();
 
 template<typename Derived>
 class opengl_test {
 protected:
-    shader                              _shader;
-    vertex_buffer                       _geometry;
-    vertex_array                        _geometry_bindings;
-    int                                 _vertices_per_object;
+    std::shared_ptr<shared_state>   _shared_state;
+    vertex_array                    _geometry_bindings;
+    shader                          _shader;
 
     void draw_object( int index_ ) {
         static_cast<Derived*>( this )->pre_draw( index_ );
 
-        glDrawArrays( GL_TRIANGLES, index_ * _vertices_per_object, _vertices_per_object );
+        glDrawArrays( GL_TRIANGLES, index_ * _shared_state->_vertices_per_object, _shared_state->_vertices_per_object );
 
         static_cast<Derived*>( this )->post_draw( index_ );
     }
 
 public:
     opengl_test() {
-        using namespace std;
-        static const float vertices[] = {
-            0.0f, 0.5f,
-            -0.5f, -0.5f,
-            0.5f, -0.5f,
-
-            0.0f + 0.7f, 0.5f,
-            -0.5f + 0.7f, -0.5f,
-            0.5f + 0.7f, -0.5f,
-
-            0.0f - 0.7f, 0.5f,
-            -0.5f - 0.7f, -0.5f,
-            0.5f - 0.7f, -0.5f
-        };
-
-        _vertices_per_object = static_cast<int>( ( end( vertices ) - begin( vertices ) ) / 2 );
+        _shared_state = get_shared_state();
 
         static_cast<Derived*>( this )->pre_init();
 
         _geometry_bindings.bind();
-        _geometry.bind( GL_ARRAY_BUFFER );
+        _shared_state->_geometry.bind( GL_ARRAY_BUFFER );
         glEnableVertexAttribArray( 0 );
         glVertexAttribPointer( _shader.get_attribute_index( "in_xyzw" ), 2, GL_FLOAT, GL_FALSE, sizeof( float ) * 2, nullptr );
-        
-        std::vector<float> buffer;
-        buffer.reserve( _vertices_per_object * 2 * NUMBER_OF_OBJECTS );
-        for ( int i = 0; i < NUMBER_OF_OBJECTS; ++i ) {
-            buffer.insert( buffer.end(), begin( vertices ), end( vertices ) );
-        }
-        glBufferData( GL_ARRAY_BUFFER, buffer.size() * sizeof( float ), buffer.data(), GL_STATIC_DRAW );
 
         static_cast<Derived*>( this )->post_init();
     }
